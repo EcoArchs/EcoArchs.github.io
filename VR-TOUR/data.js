ad3836b21501a8f0c33da3f9365d103ce983526d
GM-WH <gdaud175@gmail.com>
2025-12-15 14:58:36 +0300

    UPDATE 3

--- VR-TOUR/data.js
+++ VR-TOUR/data.js
@@ -280,8 +280,8 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "9-bmeeting-r-view-a",
-      "name": "B.Meeting R View A",
+      "id": "9-ceo-office-view-d",
+      "name": "CEO Office View D",
       "levels": [
         {
           "tileSize": 256,
@@ -311,8 +311,8 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "10-bmeeting-r-view-b",
-      "name": "B.Meeting R View B",
+      "id": "10-bmeeting-room-view-a",
+      "name": "B.Meeting Room View A",
       "levels": [
         {
           "tileSize": 256,
@@ -342,8 +342,8 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "11-bmeeting-r-view-c",
-      "name": "B.Meeting R View C",
+      "id": "11-bmeeting-room-view-b",
+      "name": "B.Meeting Room View B",
       "levels": [
         {
           "tileSize": 256,
@@ -373,7 +373,38 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "12-pool-office-view-a",
+      "id": "12-bmeeting-room-view-c",
+      "name": "B.Meeting Room View C",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-pool-office-view-a",
       "name": "Pool Office View A",
       "levels": [
         {
@@ -404,7 +435,7 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "13-pool-office-view-b",
+      "id": "14-pool-office-view-b",
       "name": "Pool Office View B",
       "levels": [
         {
@@ -435,7 +466,7 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "14-pool-office-view-c",
+      "id": "15-pool-office-view-c",
       "name": "Pool Office View C",
       "levels": [
         {
@@ -466,7 +497,7 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "15-pool-office-view-d",
+      "id": "16-pool-office-view-d",
       "name": "Pool Office View D",
       "levels": [
         {
@@ -497,7 +528,38 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "16-office-n-1-view-a",
+      "id": "17-open-kitchen-area-view-a",
+      "name": "Open Kitchen Area View A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "18-office-n-1-view-a",
       "name": "Office N-1 View A",
       "levels": [
         {
@@ -528,7 +590,7 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "17-office-n-1-view-b",
+      "id": "19-office-n-1-view-b",
       "name": "Office N-1 View B",
       "levels": [
         {
@@ -559,7 +621,7 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "18-office-n-2-view-a",
+      "id": "20-office-n-2-view-a",
       "name": "Office N-2 View A",
       "levels": [
         {
@@ -590,7 +652,7 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "19-reception-a-view-a",
+      "id": "21-reception-a-view-a",
       "name": "Reception A View A",
       "levels": [
         {
@@ -621,7 +683,7 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "20-reception-a-view-b",
+      "id": "22-reception-a-view-b",
       "name": "Reception A View B",
       "levels": [
         {
@@ -652,7 +714,7 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "21-reception-a-view-c",
+      "id": "23-reception-a-view-c",
       "name": "Reception A View C",
       "levels": [
         {
@@ -683,7 +745,7 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "22-reception-b-view-a",
+      "id": "24-reception-b-view-a",
       "name": "Reception B View A",
       "levels": [
         {
@@ -714,7 +776,7 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "23-reception-b-view-b",
+      "id": "25-reception-b-view-b",
       "name": "Reception B View B",
       "levels": [
         {
